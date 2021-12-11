7eab21cd36bd5216839e8d76cfb45ce82fb42b6c
AngelZPT <95138873+AngelZPT@users.noreply.github.com>
2021-12-11 19:20:20 -0400

    agregue un metodo a manejoentrenador

--- src/Menu/ManejoEntrenador.java
+++ src/Menu/ManejoEntrenador.java
@@ -8,9 +8,14 @@ package Menu;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
@@ -87,5 +92,55 @@ void clear(File file){
     } catch (Exception e) {
         System.out.println(e);
     }
+}
+    // esto es para buscar el entrenador en el frame actividades
+    public ArrayList LeerDatos(String idactividad)throws FileNotFoundException,
+UnsupportedEncodingException, IOException 
+{
+    boolean encontrado=false;
+    int idac=Integer.parseInt(idactividad);
+    ArrayList<String> ArrayEntrenador=new ArrayList<String>();
+    File g=new File("Entrenador.txt");
+    try
+    {
+        if(!g.exists())
+        {
+            g.createNewFile();
+        }
+        
+        Scanner s=new Scanner(g);
+        
+        while(s.hasNextLine()&& !encontrado)
+            
+        {
+            String linea=s.nextLine();
+            Scanner sl=new Scanner(linea);
+            sl.useDelimiter("\\s*;\\s*");
+            int idd=Integer.parseInt(sl.next());
+            if(idd==idac)
+            {
+            
+            String nombre=sl.next();
+            String descripcion=sl.next();
+            String localizacion=sl.next();
+            String entrenador=sl.next();
+            ArrayEntrenador.add("1");
+            ArrayEntrenador.add(nombre);
+
+            }
+                    
+        
+ 
+        }
+        s.close();
+        
+        
+    }catch(Exception e)
+    {
+        System.out.println("ERROR "+e);
+    }
+              
+    return ArrayEntrenador;
+
 } //fin del clear
 }//fin del manejo
